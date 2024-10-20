ee7b1bee8afcd783a1f09fdf93a91a6f87a24267
катя <ekat0912g@gmail.com>
2024-10-20 23:54:09 +0300

    Изменила функции у кнопок

--- xrestikiNoliki/Form1.Designer.cs
+++ xrestikiNoliki/Form1.Designer.cs
@@ -57,6 +57,7 @@
             this.button2.Size = new System.Drawing.Size(106, 101);
             this.button2.TabIndex = 1;
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button_Click);
             // 
             // button3
             // 
@@ -65,6 +66,7 @@
             this.button3.Size = new System.Drawing.Size(106, 101);
             this.button3.TabIndex = 2;
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_Click);
             // 
             // button4
             // 
@@ -73,6 +75,7 @@
             this.button4.Size = new System.Drawing.Size(106, 101);
             this.button4.TabIndex = 3;
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button_Click);
             // 
             // button5
             // 
@@ -81,6 +84,7 @@
             this.button5.Size = new System.Drawing.Size(106, 101);
             this.button5.TabIndex = 4;
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button_Click);
             // 
             // button6
             // 
@@ -89,6 +93,7 @@
             this.button6.Size = new System.Drawing.Size(106, 101);
             this.button6.TabIndex = 5;
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Button_Click);
             // 
             // button7
             // 
@@ -97,6 +102,7 @@
             this.button7.Size = new System.Drawing.Size(106, 101);
             this.button7.TabIndex = 6;
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.Button_Click);
             // 
             // button8
             // 
@@ -105,6 +111,7 @@
             this.button8.Size = new System.Drawing.Size(106, 101);
             this.button8.TabIndex = 7;
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.Button_Click);
             // 
             // button9
             // 
@@ -113,6 +120,7 @@
             this.button9.Size = new System.Drawing.Size(106, 101);
             this.button9.TabIndex = 8;
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.Button_Click);
             // 
             // button10
             // 
@@ -122,6 +130,7 @@
             this.button10.TabIndex = 9;
             this.button10.Text = "Начать заново";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // label1
             // 
