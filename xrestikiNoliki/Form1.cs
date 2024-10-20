61cf120fb448147a5039dbc641221d586512d0b2
катя <ekat0912g@gmail.com>
2024-10-20 23:19:06 +0300

    Инициализация формы и инициализация дополнительных значений

--- xrestikiNoliki/Form1.cs
+++ xrestikiNoliki/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,32 @@ namespace xrestikiNoliki
 {
     public partial class Form1 : Form
     {
+        string t = "";
+        Random rnd = new Random();
+        int[] array = new int[9];
+        Button[] btn = new Button[9];
         public Form1()
         {
             InitializeComponent();
+            if (rnd.Next(1, 10) <= 5)
+            {
+                t = "Крестики";
+                label1.Text = "Ходят X";
+            }
+            else
+            {
+                t = "1";
+                label1.Text = "Ходят 0";
+            }
+            btn[0] = button1;
+            btn[1] = button2;
+            btn[2] = button3;
+            btn[3] = button4;
+            btn[4] = button5;
+            btn[5] = button6;
+            btn[6] = button7;
+            btn[7] = button8;
+            btn[8] = button9;
         }
     }
 }
